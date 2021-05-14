UzixLS/zx-sizif-512

commit 93bcacafd279792765609a2789385e61709fdf44

refactor cpld project: split everything into modules

cpld/rtl/ports.sv
@@ -1,0 +1,174 @@
+import common::*;
+module ports(
+    input rst_n,
+    input clk28,
+    input en_128k,
+    input en_plus3,
+    input en_profi,
+    input en_kempston,
+    input en_sinclair,
+
+    cpu_bus bus,
+    output [7:0] d_out,
+    output d_out_active,
+
+    input timings_t timings,
+    input clkcpu_ck,
+    input screen_loading,
+    input [7:0] attr_next,
+    input [4:0] kd,
+    input [7:0] kempston_data,
+    input magic_active_next,
+    input tape_in,
+
+    output reg tape_out,
+    output reg beeper,
+    output reg [2:0] border,
+    output reg screenpage,
+    output reg rompage128,
+    output reg [2:0] rampage128,
+    output reg [3:0] rampage_ext,
+    output reg [2:0] port_1ffd,
+    output reg port_dffd_d3,
+    output reg port_dffd_d4,
+
+    output reg plus3_drd,
+    output reg plus3_dwr,
+    output reg plus3_mtr
+);
+
+
+/* PORT #FF */
+wire [7:0] port_ff_data = attr_next;
+reg port_ff_rd;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        port_ff_rd <= 0;
+    else
+        port_ff_rd <= bus.rd && bus.ioreq && (timings != TIMINGS_PENT || bus.a[7:0] == 8'hFF) && screen_loading;
+end
+
+
+/* PORT #FE */
+wire port_fe_cs = bus.ioreq && bus.a[0] == 0;
+reg port_fe_rd;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        port_fe_rd <= 0;
+    else
+        port_fe_rd <= port_fe_cs && bus.rd;
+end
+
+reg [4:0] kd0;
+wire [7:0] port_fe_data = {~magic_active_next, tape_in, 1'b1, kd0};
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        beeper <= 0;
+        tape_out <= 0;
+        border <= 0;
+    end
+    else if (port_fe_cs && bus.wr && clkcpu_ck) begin // clkcpu_ck to synchronize border
+        beeper <= bus.d[4];
+        tape_out <= bus.d[3];
+        border <= bus.d[2:0];
+    end
+end
+
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        kd0 <= 5'b11111;
+    end
+    else if (en_sinclair) begin
+        kd0 <= kd
+            & (bus.a[12] == 0? {~kempston_data[1], ~kempston_data[0], ~kempston_data[2], ~kempston_data[3], ~kempston_data[4]} : 5'b11111) // 6-0 keys
+            & (bus.a[15] == 0? {1'b1, ~kempston_data[6], ~kempston_data[5], 2'b11} : 5'b11111 ) ; // b-space keys
+    end
+    else begin
+        kd0 <= kd;
+    end
+end
+
+
+/* PORT #7FFD */
+wire port_7ffd_cs = en_128k && bus.ioreq && bus.a[1] == 0 && bus.a[15] == 0 && (bus.a[14] == 1'b1 || !en_plus3);
+reg lock_7ffd;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        rampage128 <= 0;
+        screenpage <= 0;
+        rompage128 <= 0;
+        lock_7ffd <= 0;
+    end
+    else if (port_7ffd_cs && bus.wr && (lock_7ffd == 0 || port_dffd_d4 == 1'b1)) begin
+        rampage128 <= bus.d[2:0];
+        screenpage <= bus.d[3];
+        rompage128 <= bus.d[4];
+        lock_7ffd <= bus.d[5];
+    end
+end
+
+
+/* PORT #DFFD */
+wire port_dffd_cs = en_profi && bus.ioreq && bus.a == 16'hDFFD;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        rampage_ext <= 0;
+        port_dffd_d3 <= 0;
+        port_dffd_d4 <= 0;
+    end
+    else if (port_dffd_cs && bus.wr) begin
+        rampage_ext <= bus.d[2:0];
+        port_dffd_d3 <= bus.d[3];
+        port_dffd_d4 <= bus.d[4];
+    end
+end
+
+
+/* PORT #1FFD */
+wire port_1ffd_cs = en_plus3 && bus.ioreq && bus.a == 16'h1FFD;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        port_1ffd <= 0;
+        plus3_mtr <= 1'b1;
+    end
+    else if (port_1ffd_cs && bus.wr) begin
+        port_1ffd <= bus.d[2:0];
+        plus3_mtr <= bus.d[3]? 1'b0 : 1'b1;
+    end
+end
+
+
+/* PORTS #2FFD & #3FFD (+3DOS) */
+wire port_2ffd_3ffd_cs = en_plus3 && bus.ioreq && (bus.a == 16'h2FFD || bus.a == 16'h3FFD);
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        plus3_dwr <= 1'b1;
+        plus3_drd <= 1'b1;
+    end
+    else begin
+        plus3_drd <= (bus.rd && port_2ffd_3ffd_cs)? 1'b0 : 1'b1;
+        plus3_dwr <= (bus.wr && port_2ffd_3ffd_cs)? 1'b0 : 1'b1;
+    end
+end
+
+
+/* KEMPSTON */
+reg kempston_rd;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        kempston_rd <= 0;
+    else
+        kempston_rd <= en_kempston && bus.ioreq && bus.rd && bus.a[7:5] == 3'b000;
+end
+
+
+/* BUS CONTROLLER */
+assign d_out_active = port_fe_rd | port_ff_rd | kempston_rd;
+
+assign d_out = 
+    kempston_rd? kempston_data :
+    port_fe_rd? port_fe_data :
+    port_ff_data ;
+
+
+endmodule

cpld/rtl/top.sv
@@ -1,0 +1,506 @@
+// `define REV_C
+// `define REV_D
+
+import common::*;
+module zx_ula (
+    input rst_n,
+    input clk28,
+
+    output n_rstcpu,
+    output clkcpu,
+    output n_clkcpu,
+
+    input [15:0] xa,
+    inout [7:0] xd,
+    output [18:0] va,
+    inout [7:0] vd,
+    output [17:14] ra,
+
+    output n_vrd,
+    output n_vwr,
+    output n_romcs,
+
+    input n_rd,
+    input n_wr,
+    input n_mreq,
+    input n_iorqge,
+    input n_m1,
+    input n_rfsh,
+    output n_int,
+    output n_nmi,
+
+    input [4:0] kd,
+    input tape_in,
+
+    input n_magic,
+
+    output [1:0] r,
+    output [1:0] g,
+    output [1:0] b,
+    output csync,
+    output hsync,
+    output vsync,
+
+    output ay_clk,
+    output ay_bdir,
+    output ay_bc1,
+    output ay_abc,
+    output ay_mono,
+
+    output snd_l,
+    output snd_r,
+
+    input n_joy_down,
+    input n_joy_right,
+    input n_joy_left,
+    input n_joy_up,
+    input n_joy_b1,
+    input n_joy_b2,
+    output joy_sel,
+
+`ifndef REV_C
+    input ps2_clk,
+    input ps2_dat,
+`endif
+
+    input sd_cd,
+    input sd_miso,
+    output sd_mosi,
+    output sd_sck,
+    output sd_cs,
+
+    output plus3_drd,
+    output plus3_dwr,
+    output plus3_mtr
+);
+
+
+/* RESET */
+reg rst_n0 = 0;
+reg [2:0] rst_n0_cnt = 0;
+always @(posedge clk28) begin
+    if (rst_n == 1'b0) begin
+        if (! (&rst_n0_cnt))
+            rst_n0_cnt <= rst_n0_cnt + 1'b1;
+    end
+    else begin
+        rst_n0_cnt <= 0;
+    end
+    rst_n0 <= ~&rst_n0_cnt;
+end
+
+
+/* SHARED DEFINITIONS */
+timings_t timings;
+turbo_t turbo;
+wire joy_pause, joy_mode;
+wire pause = joy_pause;
+wire [2:0] border;
+wire magic_beeper;
+wire up_en;
+wire clkwait;
+wire [2:0] rampage128;
+wire div_wait;
+
+
+/* CPU BUS */
+cpu_bus bus();
+reg n_iorqge_delayed;
+always @(posedge clk28)
+    n_iorqge_delayed <= n_iorqge;
+assign bus.a = xa;
+assign bus.d = xd;
+assign bus.iorq = ~n_iorqge;
+assign bus.mreq = ~n_mreq;
+assign bus.m1 = ~n_m1;
+assign bus.rfsh = ~n_rfsh;
+assign bus.rd = ~n_rd;
+assign bus.wr = ~n_wr;
+assign bus.ioreq = ~(~n_m1 | n_iorqge | n_iorqge_delayed);
+
+
+/* SCREEN CONTROLLER */
+wire blink;
+wire [2:0] screen_border = {border[2] ^ ~sd_cs, border[1] ^ magic_beeper, border[0] ^ (pause & blink)};
+wire [2:0] r0, g0;
+wire [1:0] b0;
+wire screen_fetch, screen_fetch_up, screen_loading;
+wire [14:0] screen_addr;
+wire [5:0] screen_up_addr;
+wire [7:0] attr_next;
+wire [8:0] vc, hc;
+wire [4:0] blink_cnt;
+wire clk14, clk7, clk35, ck14, ck7, ck35;
+screen screen0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+
+    .timings(timings),
+    .border(screen_border),
+    .up_en(up_en),
+
+    .r(r0),
+    .g(g0),
+    .b(b0),
+    .csync(csync),
+    .vsync(vsync),
+    .hsync(hsync),
+
+    .fetch_allow((!bus.iorq && !bus.mreq) || bus.rfsh || clkwait),
+    .fetch(screen_fetch),
+    .fetch_up(screen_fetch_up),
+    .addr(screen_addr),
+    .up_addr(screen_up_addr),
+    .fetch_data(vd),
+
+    .loading(screen_loading),
+    .blink(blink),
+    .attr_next(attr_next),
+
+    .vc_out(vc),
+    .hc_out(hc),
+    .blink_cnt(blink_cnt),
+    .clk14(clk14),
+    .clk7(clk7),
+    .clk35(clk35),
+    .ck14(ck14),
+    .ck7(ck7),
+    .ck35(ck35)
+);
+
+
+/* RGB OUTPUT */
+rgb rgb0(
+    .clk28(clk28),
+    .clk14(clk14),
+    .up_en(up_en),
+    .r_i(r0),
+    .g_i(g0),
+    .b_i(b0),
+    .r_o(r),
+    .g_o(g),
+    .b_o(b)
+);
+
+
+/* JOYSTICK / GAMEPAD */
+wire joy_up, joy_down, joy_left, joy_right, joy_start, joy_b1_turbo, joy_b2_turbo, joy_b3_turbo;
+joysega joysega0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+
+    .vc(vc),
+    .hc(hc),
+    .turbo_strobe(blink_cnt[1]),
+
+    .n_joy_up(n_joy_up),
+    .n_joy_down(n_joy_down),
+    .n_joy_left(n_joy_left),
+    .n_joy_right(n_joy_right),
+    .n_joy_b1(n_joy_b1),
+    .n_joy_b2(n_joy_b2),
+    .joy_sel(joy_sel),
+
+    .joy_up(joy_up),
+    .joy_down(joy_down),
+    .joy_left(joy_left),
+    .joy_right(joy_right),
+    .joy_b1_turbo(joy_b1_turbo),
+    .joy_b2_turbo(joy_b2_turbo),
+    .joy_b3_turbo(joy_b3_turbo),
+    .joy_start(joy_start),
+    .joy_mode(joy_mode),
+    .pause(joy_pause)
+);
+
+wire [7:0] kempston_data = {1'b0, joy_b3_turbo, joy_b2_turbo, joy_b1_turbo, joy_up, joy_down, joy_left, joy_right};
+
+
+/* CPU CONTROLLER */
+wire n_int_next, clkcpu_ck, snow, int_vector_rd;
+wire [7:0] int_vector_data;
+wire n_rstcpu0;
+assign n_rstcpu = n_rstcpu0? 1'bz : 1'b0;
+assign n_clkcpu = ~clkcpu;
+cpucontrol cpucontrol0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+    .clk14(clk14),
+    .clk7(clk7),
+    .clk35(clk35),
+
+    .bus(bus),
+
+    .vc(vc),
+    .hc(hc),
+    .rampage128(rampage128),
+    .screen_loading(screen_loading),
+    .turbo(turbo),
+    .timings(timings),
+    .pause(pause),
+    .ext_wait_cycle(div_wait || up_en),
+
+    .n_rstcpu(n_rstcpu0),
+    .clkcpu(clkcpu),
+    .clkcpu_ck(clkcpu_ck),
+    .clkwait(clkwait),
+    .n_int(n_int),
+    .n_int_next(n_int_next),
+    .snow(snow),
+    .int_vector_rd(int_vector_rd),
+    .int_vector_data(int_vector_data)
+);
+
+
+/* MAGIC */
+wire magic_mode, magic_map;
+wire magic_active_next;
+wire n_nmi0;
+assign n_nmi = n_nmi0? 1'bz : 1'b0;
+wire extlock, joy_sinclair, rom_plus3, rom_alt48;
+magic magic0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+
+    .bus(bus),
+    .n_int(n_int),
+    .n_int_next(n_int_next),
+    .n_nmi(n_nmi0),
+
+    .magic_button(n_magic == 0 || joy_mode),
+
+    .magic_mode(magic_mode),
+    .magic_map(magic_map),
+    .magic_active_next(magic_active_next),
+
+    .extlock(extlock),
+    .magic_beeper(magic_beeper),
+    .timings(timings),
+    .turbo(turbo),
+    .joy_sinclair(joy_sinclair),
+    .rom_plus3(rom_plus3),
+    .rom_alt48(rom_alt48),
+    .ay_abc(ay_abc),
+    .ay_mono(ay_mono)
+);
+
+
+/* PORTS */
+wire [7:0] ports_dout;
+wire ports_dout_active;
+wire beeper, tape_out;
+wire screenpage;
+wire rompage128;
+wire [3:0] rampage_ext;
+wire [2:0] port_1ffd;
+wire port_dffd_d3;
+wire port_dffd_d4;
+wire plus3_mtr0;
+assign plus3_mtr = plus3_mtr0? 1'bz : 1'b0;
+ports ports0 (
+    .rst_n(rst_n0),
+    .clk28(clk28),
+
+    .bus(bus),
+    .d_out(ports_dout),
+    .d_out_active(ports_dout_active),
+
+    .en_128k(1'b1),
+    .en_plus3(rom_plus3),
+    .en_profi(!extlock),
+    .en_kempston(!joy_sinclair),
+    .en_sinclair(joy_sinclair),
+
+    .timings(timings),
+    .clkcpu_ck(clkcpu_ck),
+    .screen_loading(screen_loading),
+    .attr_next(attr_next),
+    .kd(kd),
+    .kempston_data(kempston_data),
+    .magic_active_next(magic_active_next),
+    .tape_in(tape_in),
+
+    .tape_out(tape_out),
+    .beeper(beeper),
+    .border(border),
+    .screenpage(screenpage),
+    .rompage128(rompage128),
+    .rampage128(rampage128),
+    .rampage_ext(rampage_ext),
+    .port_1ffd(port_1ffd),
+    .port_dffd_d3(port_dffd_d3),
+    .port_dffd_d4(port_dffd_d4),
+
+    .plus3_drd(plus3_drd),
+    .plus3_dwr(plus3_dwr),
+    .plus3_mtr(plus3_mtr0)
+);
+
+
+/* AY */
+ay ay0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+    .bus(bus),
+    .ck35(ck35 && !pause),
+    .ay_clk(ay_clk),
+    .ay_bc1(ay_bc1),
+    .ay_bdir(ay_bdir)
+);
+
+
+/* COVOX & SOUNDRIVE */
+wire [7:0] soundrive_l0, soundrive_l1, soundrive_r0, soundrive_r1;
+soundrive soundrive0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+    .en_covox(!extlock),
+    .en_soundrive(!extlock),
+
+    .bus(bus),
+
+    .ch_l0(soundrive_l0),
+    .ch_l1(soundrive_l1),
+    .ch_r0(soundrive_r0),
+    .ch_r1(soundrive_r1)
+);
+
+
+/* SOUND MIXER */
+mixer mixer0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+
+    .beeper(beeper),
+    .tape_out(tape_out),
+    .tape_in(tape_in ^ sd_cs ^ magic_beeper),
+    .sd_l0(soundrive_l0),
+    .sd_l1(soundrive_l1),
+    .sd_r0(soundrive_r0),
+    .sd_r1(soundrive_r1),
+
+    .dac_l(snd_l),
+    .dac_r(snd_r)
+);
+
+
+/* DIVMMC */
+wire div_map, div_ram, div_ramwr_mask, div_dout_active;
+wire [7:0] div_dout;
+wire [3:0] div_page;
+divmmc divmmc0(
+    .rst_n(rst_n0),
+    .clk28(clk28),
+    .ck14(ck14),
+    .ck7(ck7),
+    .en(!extlock),
+
+    .bus(bus),
+    .d_out(div_dout),
+    .d_out_active(div_dout_active),
+
+    .sd_cd(sd_cd),
+    .sd_miso(sd_miso),
+    .sd_mosi(sd_mosi),
+    .sd_sck(sd_sck),
+    .sd_cs(sd_cs),
+    
+    .rammap(port_dffd_d4 | port_1ffd[0]),
+    .magic_mode(magic_mode),
+    .magic_map(magic_map),
+
+    .div_page(div_page),
+    .div_map(div_map),
+    .div_ram(div_ram),
+    .div_ramwr_mask(div_ramwr_mask),
+    .div_wait(div_wait)
+);
+
+
+/* ULAPLUS */
+wire up_dout_active;
+wire [7:0] up_dout;
+wire up_write_req;
+wire [5:0] up_write_addr;
+ulaplus ulaplus0(
+    .rst_n(rst_n),
+    .clk28(clk28),
+    .en(!extlock),
+    
+    .bus(bus),
+    .d_out(up_dout),
+    .d_out_active(up_dout_active),
+    
+    .active(up_en),
+    .write_req(up_write_req),
+    .write_addr(up_write_addr)
+);
+
+
+/* MEMORY CONTROLLER */
+reg romreq, ramreq, ramreq_wr;
+always @(posedge clk28 or negedge rst_n0) begin
+    if (!rst_n0) begin
+        romreq = 1'b0;
+        ramreq = 1'b0;
+        ramreq_wr = 1'b0;
+    end
+    else begin
+        romreq =  bus.mreq && !bus.rfsh &&  bus.a[14] == 0 && bus.a[15] == 0 &&
+            (magic_map || (!div_ram && div_map) || (!div_ram && !port_dffd_d4 && !port_1ffd[0]));
+        ramreq = (bus.mreq && !bus.rfsh && !romreq) || up_write_req;
+        ramreq_wr = ramreq && bus.wr && div_ramwr_mask == 0;
+    end
+end
+
+assign n_romcs = (romreq && bus.mreq)? 1'b0 : 1'b1;
+assign n_vrd = ((ramreq && bus.rd) || screen_fetch)? 1'b0 : 1'b1;
+assign n_vwr = (ramreq_wr && bus.wr && !screen_fetch)? 1'b0 : 1'b1;
+
+reg [18:13] ram_a;
+always @(posedge clk28) begin
+    ram_a <=
+        magic_map & bus.a[15] & bus.a[14]? {2'b00, 3'b111, bus.a[13]} :
+        magic_map? {3'b111, screenpage, bus.a[14:13]} :
+        div_map & ~bus.a[14] & ~bus.a[15] & bus.a[13]? {2'b01, div_page} :
+        div_map & ~bus.a[14] & ~bus.a[15]? {2'b01, 4'b0011} :
+        port_dffd_d3 & bus.a[15]? {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} :
+        port_dffd_d3 & bus.a[14]? {~rampage_ext[1:0], rampage128, bus.a[13]} :
+        (port_1ffd[2] == 1'b0 && port_1ffd[0] == 1'b1)? {2'b11, port_1ffd[1], bus.a[15], bus.a[14], bus.a[13]} :
+        (port_1ffd == 3'b101)? {2'b11, ~(bus.a[15] & bus.a[14]), bus.a[15], bus.a[14]} :
+        (port_1ffd == 3'b111)? {2'b11, ~(bus.a[15] & bus.a[14]), (bus.a[15] | bus.a[14]), bus.a[14]} :
+        bus.a[15] & bus.a[14]? {~rampage_ext[1:0], rampage128, bus.a[13]} :
+        {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} ;
+end
+
+assign ra[17:14] =
+    magic_map? 3'd2 :
+    div_map? 3'd3 :
+    (rom_plus3 && port_1ffd[2] == 1'b0 && rompage128 == 1'b0)? 3'd4 :
+    (rom_plus3 && port_1ffd[2] == 1'b0 && rompage128 == 1'b1)? 3'd5 :
+    (rom_plus3 && port_1ffd[2] == 1'b1 && rompage128 == 1'b0)? 3'd6 :
+    (rompage128 == 1'b1 && rom_alt48 == 1'b1)? 3'd7 :
+    (rompage128 == 1'b1)? 3'd1 :
+    3'd0;
+
+assign va[18:0] =
+    screen_fetch && screen_fetch_up? {2'b00, 3'b111, 8'b11111111, screen_up_addr} :
+    screen_fetch && snow? {3'b111, screenpage, screen_addr[14:8], bus.a[7:0]} :
+    screen_fetch? {3'b111, screenpage, screen_addr} :
+    up_write_req? {2'b00, 3'b111, 8'b11111111, up_write_addr} :
+    {ram_a[18:13], bus.a[12:0]};
+
+assign xd[7:0] =
+    ramreq && bus.rd? vd :
+    up_dout_active? up_dout :
+    div_dout_active? div_dout :
+    ports_dout_active? ports_dout :
+    int_vector_rd? int_vector_data :
+    {8{1'bz}};
+
+assign vd[7:0] =
+    n_vrd == 0? {8{1'bz}} :
+    xd;
+
+
+endmodule

cpld/old/rev.A/testbench_zx_ula.rev.A.v
@@ -1,5 +1,5 @@
 `define USE_FPGA
-`include "../cpld/rev.A/top.v"
+`include "top.v"
 
 `timescale 100ps/10ps 
 module testbench_zx_ula();

cpld/rtl/ay.sv
@@ -1,0 +1,30 @@
+import common::*;
+module ay(
+    input rst_n,
+    input clk28,
+
+    cpu_bus bus,
+    input ck35,
+
+    output reg ay_clk,
+    output reg ay_bc1,
+    output reg ay_bdir
+);
+
+
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        ay_clk <= 0;
+        ay_bc1 <= 0;
+        ay_bdir <= 0;
+    end
+    else begin
+        if (ck35)
+            ay_clk <= ~ay_clk;
+        ay_bc1  <= bus.a[15] == 1'b1 && bus.a[14] == 1'b1 && bus.a[1] == 0 && bus.ioreq;
+        ay_bdir <= bus.a[15] == 1'b1 && bus.a[1] == 0 && bus.ioreq && bus.wr;
+    end
+end
+
+
+endmodule

cpld/rtl/common.sv
@@ -1,0 +1,18 @@
+package common;
+    typedef enum { TIMINGS_PENT, TIMINGS_S128, TIMINGS_S48 } timings_t;
+    typedef enum { TURBO_NONE, TURBO_7, TURBO_14 = 3 } turbo_t;
+endpackage
+
+
+interface cpu_bus();
+    wire [15:0] a;
+    wire [7:0] d;
+    wire iorq;
+    wire mreq;
+    wire m1;
+    wire rfsh;
+    wire rd;
+    wire wr;
+
+    reg ioreq;
+endinterface

cpld/rtl/cpucontrol.sv
@@ -1,0 +1,111 @@
+import common::*;
+module cpucontrol(
+    input rst_n,
+    input clk28,
+    input clk14,
+    input clk7,
+    input clk35,
+
+    cpu_bus bus,
+
+    output int_vector_rd,
+    output [7:0] int_vector_data,
+
+    input [8:0] vc,
+    input [8:0] hc,
+    input [2:0] rampage128,
+    input screen_loading,
+    input turbo_t turbo,
+    input timings_t timings,
+    input pause,
+    input ext_wait_cycle,
+
+    output reg n_rstcpu,
+    output reg clkcpu,
+    output clkcpu_ck,
+    output clkwait,
+    output reg n_int,
+    output n_int_next,
+    output snow
+);
+
+
+/* CONTENTION */
+reg mreq_delayed, iorq_delayed;
+always @(posedge clkcpu)
+    mreq_delayed <= bus.mreq;
+always @(posedge clkcpu)
+    iorq_delayed <= bus.iorq;
+wire contention_mem_addr = bus.a[14] & (~bus.a[15] | (bus.a[15] & rampage128[0]));
+wire contention_mem = iorq_delayed == 1'b0 && mreq_delayed == 1'b0 && contention_mem_addr;
+wire contention_io = iorq_delayed == 1'b0 && bus.iorq;
+wire contention0 = screen_loading && (hc[2] || hc[3]) && (contention_mem || contention_io);
+wire contention = clkcpu && contention0 && turbo == TURBO_NONE && timings != TIMINGS_PENT;
+assign snow = (timings != TIMINGS_PENT) && bus.a[14] && ~bus.a[15] && bus.rfsh;
+
+
+/* CLOCK */
+reg [2:0] turbo_wait;
+wire turbo_wait_trig0 = bus.rd || bus.wr;
+reg turbo_wait_trig1;
+always @(posedge clk28) begin
+    turbo_wait_trig1 <= turbo_wait_trig0;
+    turbo_wait[0] <= turbo == TURBO_14 && turbo_wait_trig0 && !turbo_wait_trig1;
+    turbo_wait[1] <= turbo_wait[0] && (bus.iorq || ext_wait_cycle);
+    turbo_wait[2] <= turbo_wait[1];
+end
+
+reg clkcpu_prev;
+assign clkcpu_ck = clkcpu && ! clkcpu_prev;
+assign clkwait = pause || contention || (|turbo_wait);
+always @(negedge clk28) begin
+    clkcpu_prev <= clkcpu;
+    clkcpu <= clkwait? clkcpu : (turbo == TURBO_14)? clk14 : (turbo == TURBO_7)? clk7 : clk35;
+end
+
+
+/* INT GENERATOR */
+localparam INT_V_S48       = 247;
+localparam INT_H_S48       = 442;
+localparam INT_V_S128      = 247;
+localparam INT_H_S128      = 450;
+localparam INT_V_PENT      = 239;
+localparam INT_H_PENT      = 316;
+wire int_begin =
+    (timings == TIMINGS_PENT)?
+        vc == INT_V_PENT && hc == INT_H_PENT :
+    (timings == TIMINGS_S128)?
+        vc == INT_V_S128 && hc == INT_H_S128 :
+    // 48K 
+        vc == INT_V_S48 && hc == INT_H_S48 ;
+reg [4:0] int_cnt;
+assign n_int_next = (|int_cnt)? 1'b0 : 1'b1;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        int_cnt <= 0;
+        n_int <= 1'b1;
+    end
+    else begin
+        if (clkcpu_ck)
+            n_int <= n_int_next;
+        if ((int_cnt != 0 && clkcpu_ck) || (int_cnt == 0 && int_begin))
+            int_cnt <= int_cnt + 1'b1;
+    end
+end
+
+
+/* INT VECTOR */
+assign int_vector_rd = bus.iorq && bus.m1;
+assign int_vector_data = 8'hff;
+
+
+/* RESET */
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        n_rstcpu <= 0;
+    else if (vc[0])
+        n_rstcpu <= 1'b1;
+end
+
+
+endmodule

cpld/rtl/divmmc.sv
@@ -1,0 +1,138 @@
+import common::*;
+module divmmc(
+    input rst_n,
+    input clk28,
+    input ck14,
+    input ck7,
+    input en,
+
+    cpu_bus bus,
+    output [7:0] d_out,
+    output d_out_active,
+
+    input sd_cd,
+    input sd_miso,
+    output sd_mosi,
+    output reg sd_sck,
+    output reg sd_cs,
+
+    input rammap,
+    input magic_mode,
+    input magic_map,
+
+    output reg [3:0] div_page,
+    output div_map,
+    output div_ram,
+    output div_ramwr_mask,
+    output div_wait
+);
+
+reg div_automap, div_automap_next;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        div_automap_next <= 0;
+        div_automap <= 0;
+    end
+    else if (bus.m1 && bus.mreq && magic_map == 0) begin 
+        if (sd_cd || !en || rammap) begin
+            div_automap_next <= 0;
+        end
+        else if (bus.a[15:3] == 13'h3FF) begin // exit vectors 1FF8-1FFF
+            div_automap_next <= 0;
+        end
+        else if (
+                bus.a == 16'h0000 || // power-on/reset/rst0/software restart
+                bus.a == 16'h0008 || // syntax error
+                bus.a == 16'h0038 || // im1 interrupt/rst #38
+                (bus.a == 16'h0066 && !magic_mode) || // nmi routine
+                bus.a == 16'h04C6 || // tape save routine
+                bus.a == 16'h0562    // tape load and verify routine
+                ) begin
+            div_automap_next <= 1'b1;
+        end
+        else if (bus.a[15:8] == 8'h3D) begin // tr-dos mapping area
+            div_automap_next <= 1'b1;
+            div_automap <= 1'b1;
+        end
+    end
+    else if (!bus.m1) begin
+        div_automap <= div_automap_next;
+    end
+end
+
+reg spi_rd;
+reg div_conmem, div_mapram;
+wire port_e3_cs = en && bus.ioreq && bus.a[7:0] == 8'hE3;
+wire port_e7_cs = en && bus.ioreq && bus.a[7:0] == 8'hE7;
+wire port_eb_cs = en && bus.ioreq && bus.a[7:0] == 8'hEB;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        spi_rd <= 0;
+        div_page <= 0;
+        div_mapram <= 0;
+        div_conmem <= 0;
+        sd_cs <= 1'b1;
+    end
+    else begin
+        spi_rd <= port_eb_cs && bus.rd;
+        if (port_e3_cs && bus.wr) begin
+            div_page <= bus.d[3:0];
+            div_mapram <= bus.d[6] | div_mapram;
+            div_conmem <= bus.d[7];
+        end
+        if (port_e7_cs && bus.wr) begin
+            sd_cs <= bus.d[0];
+        end
+    end
+end
+
+reg [3:0] spi_cnt;
+wire spi_cnt_en = ~spi_cnt[3] | spi_cnt[2] | spi_cnt[1] | spi_cnt[0];
+assign div_wait = ~spi_cnt[3];
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        spi_cnt <= 0;
+    else if (port_eb_cs && (bus.rd || bus.wr))
+        spi_cnt <= 4'b1110;
+    else if (spi_cnt_en && ck7)
+        spi_cnt <= spi_cnt + 1'b1;
+end
+
+reg spi_mosi_en;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        spi_mosi_en <= 0;
+    else if (port_eb_cs && bus.wr)
+        spi_mosi_en <= 1'b1;
+    else if (!spi_cnt_en)
+        spi_mosi_en <= 0;
+end
+
+reg [7:0] spi_reg;
+assign sd_mosi = spi_mosi_en? spi_reg[7] : 1'b1;
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n)
+        spi_reg <= 0;
+    else if (port_eb_cs && bus.wr)
+        spi_reg <= bus.d;
+    else if (spi_cnt[3] == 1'b0 && ck7)
+        spi_reg[7:0] <= {spi_reg[6:0], sd_miso};
+end
+
+always @(posedge clk28) begin
+    if (ck14)
+        sd_sck <= ~sd_sck & ~spi_cnt[3];
+end
+
+
+assign div_map = div_automap | div_conmem;
+assign div_ram = (div_conmem == 1 && bus.a[13] == 1) ||
+    (div_automap == 1 && bus.a[13] == 1) ||
+    (div_conmem == 0 && div_automap == 1 && div_mapram == 1);
+assign div_ramwr_mask = bus.a[15] == 0 && bus.a[14] == 0 &&
+    (bus.a[13] == 0 || div_page == 4'b0011) && div_conmem == 0 && div_automap == 1 && div_mapram == 1;
+
+assign d_out_active = spi_rd;
+assign d_out = spi_reg;
+
+endmodule

cpld/rtl/joysega.v
@@ -1,0 +1,110 @@
+module joysega(
+    input clk28,
+    input rst_n,
+
+    input [8:0] vc,
+    input [8:0] hc,
+    input turbo_strobe,
+
+    input n_joy_up,
+    input n_joy_down,
+    input n_joy_left,
+    input n_joy_right,
+    input n_joy_b1,
+    input n_joy_b2,
+    output reg joy_sel,
+
+    output reg joy_up,
+    output reg joy_down,
+    output reg joy_left,
+    output reg joy_right,
+    output reg joy_b1,
+    output reg joy_b2,
+    output reg joy_b3,
+    output reg joy_x,
+    output reg joy_y,
+    output reg joy_z,
+    output reg joy_start,
+    output reg joy_mode,
+    output reg pause,
+    output wire joy_b1_turbo,
+    output wire joy_b2_turbo,
+    output wire joy_b3_turbo
+);
+
+
+reg joy_md, joy_md6;
+wire joy_rd_ena = hc < 256 && vc[6:0] == 0; // every ~8ms
+wire joy_rd_strobe = hc[4];
+wire [2:0] joy_rd_state = hc[7:5]; // one step ~4.5us
+
+always @(posedge clk28 or negedge rst_n) begin
+    if (!rst_n) begin
+        joy_md <= 0;
+        joy_md6 <= 0;
+        joy_up <= 0;
+        joy_down <= 0;
+        joy_left <= 0;
+        joy_right <= 0;
+        joy_b1 <= 0;
+        joy_b2 <= 0;
+        joy_b3 <= 0;
+        joy_start <= 0;
+        joy_mode <= 0;
+        joy_x <= 0;
+        joy_y <= 0;
+        joy_z <= 0;
+        pause <= 0;
+    end
+    else begin
+        joy_sel <= (joy_rd_state[0] && joy_rd_ena)? 1'b1 : 1'b0;
+        if (joy_rd_ena && joy_rd_strobe) begin
+            if (joy_rd_state == 3'd2) begin
+                if (n_joy_left == 0 && n_joy_right == 0) begin
+                    joy_md <= 1'b1;
+                    joy_b3 <= ~n_joy_b1;
+                    if (joy_start == 1'b0 && n_joy_b2 == 1'b0)
+                        pause <= ~pause;
+                    joy_start <= ~n_joy_b2;
+                end
+                else begin
+                    joy_md <= 0;
+                    joy_b3 <= 0;
+                    joy_start <= 0;
+                end
+            end
+            else if (joy_rd_state == 3'd3) begin
+                joy_up <= ~n_joy_up;
+                joy_down <= ~n_joy_down;
+                joy_left <= ~n_joy_left;
+                joy_right <= ~n_joy_right;
+                joy_b1 <= ~n_joy_b1;
+                joy_b2 <= ~n_joy_b2;
+            end
+            else if (joy_rd_state == 3'd4) begin
+                joy_md6 <= joy_md && n_joy_up == 0 && n_joy_down == 0;
+            end
+            else if (joy_rd_state == 3'd5) begin
+                if (joy_md6) begin
+                    joy_mode <= ~n_joy_right;
+                    joy_x <= ~n_joy_left;
+                    joy_y <= ~n_joy_down;
+                    joy_z <= ~n_joy_up;
+                end
+                else begin
+                    joy_mode <= 0;
+                    joy_x <= 0;
+                    joy_y <= 0;
+                    joy_z <= 0;
+                end
+            end
+        end
+    end
+end
+
+assign joy_b1_turbo = joy_b1 | (joy_y & turbo_strobe);
+assign joy_b2_turbo = joy_b2 | (joy_z & turbo_strobe);
+assign joy_b3_turbo = joy_b3 | (joy_x & turbo_strobe);
+
+
+endmodule

commit ed4942a754dca3ecec0e5337626a9a090a154c89

fix reserving RAM for DivMMC (broken in commit 93bcaca)

cpld/rtl/ports.sv
@@ -27,7 +27,7 @@ module ports(
     output reg screenpage,
     output reg rompage128,
     output reg [2:0] rampage128,
-    output reg [3:0] rampage_ext,
+    output reg [2:0] rampage_ext,
     output reg [2:0] port_1ffd,
     output reg port_dffd_d3,
     output reg port_dffd_d4,

cpld/rtl/top.sv
@@ -324,7 +324,7 @@ wire ports_dout_active;
 wire beeper, tape_out;
 wire screenpage;
 wire rompage128;
-wire [3:0] rampage_ext;
+wire [2:0] rampage_ext;
 wire [2:0] port_1ffd;
 wire port_dffd_d3;
 wire port_dffd_d4;
@@ -490,6 +490,9 @@ assign n_romcs = (romreq && bus.mreq)? 1'b0 : 1'b1;
 assign n_vrd = ((ramreq && bus.rd) || screen_fetch)? 1'b0 : 1'b1;
 assign n_vwr = (ramreq_wr && bus.wr && !screen_fetch)? 1'b0 : 1'b1;
 
+// reserve 128K RAM for DivMMC if sd card is insterted
+wire [1:0] rampage_ext0 = {~sd_cd? 1'b0 : rampage_ext[1], rampage_ext[0]};
+
 reg [18:13] ram_a;
 always @(posedge clk28) begin
     ram_a <=
@@ -498,11 +501,11 @@ always @(posedge clk28) begin
         div_map & ~bus.a[14] & ~bus.a[15] & bus.a[13]? {2'b01, div_page} :
         div_map & ~bus.a[14] & ~bus.a[15]? {2'b01, 4'b0011} :
         port_dffd_d3 & bus.a[15]? {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} :
-        port_dffd_d3 & bus.a[14]? {~rampage_ext[1:0], rampage128, bus.a[13]} :
+        port_dffd_d3 & bus.a[14]? {~rampage_ext0[1:0], rampage128, bus.a[13]} :
         (port_1ffd[2] == 1'b0 && port_1ffd[0] == 1'b1)? {2'b11, port_1ffd[1], bus.a[15], bus.a[14], bus.a[13]} :
         (port_1ffd == 3'b101)? {2'b11, ~(bus.a[15] & bus.a[14]), bus.a[15], bus.a[14]} :
         (port_1ffd == 3'b111)? {2'b11, ~(bus.a[15] & bus.a[14]), (bus.a[15] | bus.a[14]), bus.a[14]} :
-        bus.a[15] & bus.a[14]? {~rampage_ext[1:0], rampage128, bus.a[13]} :
+        bus.a[15] & bus.a[14]? {~rampage_ext0[1:0], rampage128, bus.a[13]} :
         {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} ;
 end
 