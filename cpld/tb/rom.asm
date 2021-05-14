8c431b45d8215385b7f11a7e652025fa863a5da6
UzixLS <uzix.ls@gmail.com>
2021-05-14 20:44:48 +0300

    testbench: handle magic in rom

--- cpld/tb/rom.asm
+++ cpld/tb/rom.asm
@@ -11,14 +11,22 @@ Int1:
 	ORG #00FF
 	DB #38
 
-	ORG #0066
+	ORG #8066     // nmi #0066 enter vector (magic page)
 Nmi:
-	jp #1ffa
-	push af
-	ld a, #08
-	out (#ff), a
-	pop af
-	retn
+	ld hl, #8000              ; which address to readout
+	ld a, #2a                 ; 2a - ld hl, (nn)
+    ld (Readout_vector-2), a  ; ...
+    ld (Readout_vector-1), hl ; hl was set earlier by im2 table address
+    ld a, #c9                 ; c9 - ret
+    ld (Readout_vector+1), a  ; ...
+    call Readout_vector-2     ; hl contains default im2 handler address now
+	jr Nmi
+
+; Magic vectors
+    ORG #F000
+Exit_vector:
+    ORG #F008
+Readout_vector:
 
 	ORG #1000
 Main:
@@ -48,24 +56,19 @@ Loop:
 	halt
 	jp Loop
 
-	ORG #1FF8 // DivROM exit vector
-	reti
-	ORG #1FFA // DivROM exit vector
-	retn
-	ORG #1FFF // DivROM exit vector
-	nop
+	ORG #1FFF      // divmmc exit vector (main page)
+	nop            // this instruction skipped normally
 	jp #3D00
 
-	ORG #C000 // mapped #0000
+	ORG #C000      // divmmc #0000 handle vector (divmmc page)
 DivROM_Start:
-	nop
+	nop            // this instruction skipped normally
+	ld bc, #00C3   // JP#0000 opcode
+	ld (#3D00), bc // store opcode at #3D00 (divmmc page)
 	ld bc, #3D00
 	push bc
-	jp #1FFF
-	ORG #DFFF // mapped #1FFF
-	nop
-	ORG #1D00 // mapped #3D00
-	jp #0000
-
+	jp #1FFF       // go to #DFFD (divmmc page)
+	ORG #DFFF      // divmmc #1FFF exit vector (divmmc page)
+	nop            // last instruction executed in this page
 
 	SAVEBIN "rom.bin",0,65536
