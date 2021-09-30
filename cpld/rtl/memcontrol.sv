UzixLS/zx-sizif-512

commit 2b0dab14d56531be4199492fac09c12423f9c35b

improve 14MHz turbo mode stability

cpld/rtl/memcontrol.sv
@@ -32,11 +32,11 @@ module memcontrol(
     cpu_bus bus,
     inout [7:0] xd,
     output [17:14] ra,
-    output n_romcs,
+    output reg n_romcs,
     output [18:0] va,
     inout [7:0] vd,
     output n_vrd,
-    output n_vwr,
+    output reg n_vwr,
 
     input machine_t machine,
     input screenpage,
@@ -74,23 +74,43 @@ module memcontrol(
     input [7:0] ports_dout
 );
 
-wire romreq =  bus.mreq && !bus.rfsh &&  bus.a[14] == 0 && bus.a[15] == 0 &&
-        (magic_map || (!div_ram && div_map) || (!div_ram && !port_dffd[4] && !port_1ffd[0]));
-wire ramreq = (bus.mreq && !bus.rfsh && !romreq) || up_write_req;
-wire ramreq_wr = ramreq && bus.wr && div_ramwr_mask == 0;
+wire [18:13] ram_a;
 
-assign n_romcs = (romreq && bus.mreq)? 1'b0 : 1'b1;
-assign n_vrd = ((ramreq && bus.rd) || screen_fetch)? 1'b0 : 1'b1;
-assign n_vwr = (ramreq_wr && bus.wr && !screen_fetch)? 1'b0 : 1'b1;
+assign n_vrd = 1'b0;
+
+reg romreq, ramreq;
+always @(posedge clk28) begin
+    romreq =  bus.mreq && !bus.rfsh && bus.a[15:14] == 2'b00 &&
+        (magic_map || (!div_ram && div_map) || (!div_ram && !port_dffd[4] && !port_1ffd[0]));
+    ramreq = (bus.mreq && !bus.rfsh && !romreq) || up_write_req;
+
+    n_vwr = ~(ramreq && bus.wr && !div_ramwr_mask && !screen_fetch);
+    n_romcs = ~romreq;
+end
+
+reg vwr0;
+reg [7:0] xd_bufwr;
+reg ramreq0;
+reg [18:0] va_buf;
+always @(negedge clk28) begin
+    if (~n_vwr && !vwr0)
+        xd_bufwr <= xd;
+    vwr0 <= ~n_vwr;
+
+    if (ramreq && !ramreq0)
+        va_buf <= {ram_a[18:13], bus.a[12:0]};
+    ramreq0 <= ramreq;
+end
+
 
 // reserve 128K RAM for DivMMC
 wire [1:0] rampage_ext0 = {divmmc_en? 1'b1 : ~rampage_ext[1], ~rampage_ext[0]};
 
-wire [18:13] ram_a =
+assign ram_a =
     magic_map & bus.a[15] & bus.a[14]? {2'b00, 3'b111, bus.a[13]} :
     magic_map? {3'b111, screenpage, bus.a[14:13]} :
-    div_map & ~bus.a[14] & ~bus.a[15] & bus.a[13]? {2'b01, div_page} :
-    div_map & ~bus.a[14] & ~bus.a[15]? {2'b01, 4'b0011} :
+    div_map & ~bus.a[15] & ~bus.a[14] & bus.a[13]? {2'b01, div_page} :
+    div_map & ~bus.a[15] & ~bus.a[14]? {2'b01, 4'b0011} :
     port_dffd[3] & bus.a[15]? {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} :
     port_dffd[3] & bus.a[14]? {rampage_ext0, rampage128, bus.a[13]} :
     (port_1ffd[2] == 1'b0 && port_1ffd[0] == 1'b1)? {2'b11, port_1ffd[1], bus.a[15], bus.a[14], bus.a[13]} :
@@ -98,6 +118,7 @@ wire [18:13] ram_a =
     (port_1ffd == 3'b111)? {2'b11, ~(bus.a[15] & bus.a[14]), (bus.a[15] | bus.a[14]), bus.a[14], bus.a[13]} :
     bus.a[15] & bus.a[14]? {rampage_ext0, rampage128, bus.a[13]} :
     {2'b11, bus.a[14], bus.a[15], bus.a[14], bus.a[13]} ;
+
 
 assign ra[17:14] =
     magic_map? `BANK_MAGIC :
@@ -132,7 +153,8 @@ assign va[18:0] =
     screen_fetch && snow? {3'b111, screenpage, screen_addr[14:8], bus.a[7:0]} :
     screen_fetch? {3'b111, screenpage, screen_addr} :
     up_write_req? {2'b00, 3'b111, 8'b11111111, up_write_addr} :
-    {ram_a[18:13], bus.a[12:0]};
+    va_buf;
+
 
 // this is required because of weak xd pullup causing garbage reads from nonexisting ports
 reg [1:0] xd_precharge0;
@@ -151,8 +173,8 @@ assign xd[7:0] =
     {8{1'bz}} ;
 
 assign vd[7:0] =
-    n_vrd == 0? {8{1'bz}} :
-    xd;
+    (~n_vwr & vwr0)? xd_bufwr :
+    {8{1'bz}};
 
 
 endmodule

cpld/tb/testbench_zx_ula.v
@@ -111,8 +111,10 @@ end
 
 reg [7:0] ram [0:524288];
 reg [7:0] ram_q;
+wire [7:0] ram_q0;
+assign #55 ram_q0 = ram_q;
 wire [18:0] ram_addr_a;
-always @(posedge clk28) begin
+always @* begin
     if (n_vwr == 0) begin
         ram[ram_addr_a] <= vd;
     end
@@ -133,7 +135,7 @@ assign rom_addr = {ra[16:14], a_cpu[13:0]};
 assign ram_addr_a = va;
 
 assign vd =
-    ~n_vrd? ram_q :
+    (~n_vrd & n_vwr)? ram_q0 :
     {8{1'bz}};
 
 assign (weak0, weak1) xd =

cpld/rtl/cpucontrol.sv
@@ -45,19 +45,22 @@ assign snow = bus.a[14] && ~bus.a[15] && bus.rfsh && (machine == MACHINE_S48 || 
 
 
 /* CLOCK */
-reg [2:0] turbo_wait;
-wire turbo_wait_trig0 = bus.rd || bus.wr;
-reg turbo_wait_trig1;
+reg [3:0] turbo_wait;
+wire turbo_wait_trig0 = turbo == TURBO_14 && bus.mreq && !bus.rfsh;
+wire turbo_wait_trig1 = turbo == TURBO_14 && (bus.rd || bus.wr);
+reg turbo_wait_trig0_prev, turbo_wait_trig1_prev;
 always @(posedge clk28) begin
-    turbo_wait[0] <= turbo == TURBO_14 && turbo_wait_trig0 && !turbo_wait_trig1;
-    turbo_wait[1] <= turbo_wait[0] && (bus.iorq || ext_wait_cycle);
-    turbo_wait[2] <= turbo_wait[1];
-    turbo_wait_trig1 <= turbo_wait_trig0;
+    turbo_wait[0] <= turbo_wait_trig0 && !turbo_wait_trig0_prev;
+    turbo_wait[1] <= turbo_wait[0] || (turbo_wait_trig1 && !turbo_wait_trig1_prev);
+    turbo_wait[2] <= turbo_wait[1] && (bus.iorq || ext_wait_cycle);
+    turbo_wait[3] <= turbo_wait[2];
+    turbo_wait_trig0_prev <= turbo_wait_trig0;
+    turbo_wait_trig1_prev <= turbo_wait_trig1;
 end
 
 reg clkcpu_prev;
 assign clkcpu_ck = clkcpu && !clkcpu_prev;
-assign clkwait = contention || (|turbo_wait);
+assign clkwait = contention || (|turbo_wait[3:1]);
 always @(posedge clk28) begin
     clkcpu_prev <= clkcpu;
     if (clkwait)

cpld/rtl/top.sv
@@ -155,7 +155,7 @@ screen screen0(
     .vsync(vsync),
     .hsync(hsync),
 
-    .fetch_allow((!up_write_req && !bus.mreq) || bus.rfsh || clkwait),
+    .fetch_allow((!up_write_req && !bus.mreq) || bus.rfsh || (clkwait && turbo == TURBO_NONE)),
     .fetch(screen_fetch),
     .fetch_up(screen_fetch_up),
     .addr(screen_addr),
@@ -277,7 +277,7 @@ cpucontrol cpucontrol0(
     .machine(machine),
     .screen_contention(screen_contention),
     .turbo(turbo),
-    .ext_wait_cycle(div_wait || up_active),
+    .ext_wait_cycle(div_wait),
 
     .n_rstcpu(n_rstcpu0),
     .clkcpu(clkcpu),

cpld/syn/clocks.sdc
@@ -1,7 +1,7 @@
 create_clock -period 28.8MHz -name {clk_28mhz} [get_ports {clk28}]
 
 # clkcpu up to 14 MHz
-create_generated_clock -name {clkcpu} -divide_by 4 -source [get_ports {clk28}] [get_registers {cpucontrol:cpucontrol0|clkcpu}]
+create_generated_clock -name {clkcpu} -divide_by 2 -source [get_ports {clk28}] [get_registers {cpucontrol:cpucontrol0|clkcpu}]
 
 derive_clock_uncertainty
 derive_clocks -period 7.2MHz

commit 7828fa63c69b7fca630be858aff0e2be129a9bb9

fix random ram errors (broken in 2b0dab1)

cpld/rtl/memcontrol.sv
@@ -90,16 +90,14 @@ end
 
 reg vwr0;
 reg [7:0] xd_bufwr;
-reg ramreq0;
 reg [18:0] va_buf;
 always @(negedge clk28) begin
     if (~n_vwr && !vwr0)
         xd_bufwr <= xd;
     vwr0 <= ~n_vwr;
 
-    if (ramreq && !ramreq0)
+    if (bus.mreq && ramreq)
         va_buf <= {ram_a[18:13], bus.a[12:0]};
-    ramreq0 <= ramreq;
 end
 
 

cpld/tb/testbench_zx_ula.v
@@ -7,7 +7,8 @@ reg clk28;
 
 
 /* CPU */
-wire clkcpu;
+wire clkcpu, clkcpu0;
+assign #5 clkcpu0 = clkcpu;
 wire n_rstcpu;
 wire [15:0] a_cpu, a_cpu_cpu;
 wire [7:0] d_cpu_o, d_cpu_i;
@@ -23,7 +24,7 @@ wire n_nmi, n_nmi0;
 
 T80na cpu1(
     .RESET_n(n_rstcpu),
-    .CLK_n(clkcpu),
+    .CLK_n(clkcpu0),
     .WAIT_n(n_wait_cpu),
     .INT_n(n_int),
     .NMI_n(n_nmi0),
@@ -123,7 +124,7 @@ end
 initial begin
     integer i;
     for (i = 0; i < 524288; i++)
-        ram[i] <= 0;
+        ram[i] <= i[7:0];
 end
 
 
@@ -159,13 +160,13 @@ assign d_cpu_i =
 // assign a_cpu = a_cpu_cpu;
 
 /* CPU SIGNALS (Z84C0020 timings) */
-// assign #40 n_rd = n_rd_cpu; //TdCf(RDf)
-// assign #40 n_wr = n_wr_cpu; //TdCf(WRf)
-// assign #40 n_iorq = n_iorq_cpu; //TdCr(IORQf)
-// assign #40 n_mreq = n_mreq_cpu; //TdCf(MREQf)
-// assign #45 n_m1 = n_m1_cpu; //TdCr(M1f)
-// assign #60 n_rfsh = n_rfsh_cpu; //TdCr(RFSHf)
-// assign #57 a_cpu = a_cpu_cpu; //TdCr(A)
+assign #40 n_rd = n_rd_cpu; //TdCf(RDf)
+assign #40 n_wr = n_wr_cpu; //TdCf(WRf)
+assign #40 n_iorq = n_iorq_cpu; //TdCr(IORQf)
+assign #40 n_mreq = n_mreq_cpu; //TdCf(MREQf)
+assign #45 n_m1 = n_m1_cpu; //TdCr(M1f)
+assign #60 n_rfsh = n_rfsh_cpu; //TdCr(RFSHf)
+assign #57 a_cpu = a_cpu_cpu; //TdCr(A)
 
 /* CPU SIGNALS (Z84C0010 timings) */
 // assign #65 n_rd = n_rd_cpu; //TdCf(RDf)
@@ -177,13 +178,13 @@ assign d_cpu_i =
 // assign #65 a_cpu = a_cpu_cpu; //TdCr(A)
 
 /* CPU SIGNALS (Z84C0008 timings) */
-assign #70 n_rd = n_rd_cpu; //TdCf(RDf)
-assign #60 n_wr = n_wr_cpu; //TdCf(WRf)
-assign #55 n_iorq = n_iorq_cpu; //TdCr(IORQf)
-assign #60 n_mreq = n_mreq_cpu; //TdCf(MREQf)
-assign #70 n_m1 = n_m1_cpu; //TdCr(M1f)
-assign #95 n_rfsh = n_rfsh_cpu; //TdCr(RFSHf)
-assign #80 a_cpu = a_cpu_cpu; //TdCr(A)
+// assign #70 n_rd = n_rd_cpu; //TdCf(RDf)
+// assign #60 n_wr = n_wr_cpu; //TdCf(WRf)
+// assign #55 n_iorq = n_iorq_cpu; //TdCr(IORQf)
+// assign #60 n_mreq = n_mreq_cpu; //TdCf(MREQf)
+// assign #70 n_m1 = n_m1_cpu; //TdCr(M1f)
+// assign #95 n_rfsh = n_rfsh_cpu; //TdCr(RFSHf)
+// assign #80 a_cpu = a_cpu_cpu; //TdCr(A)
 
 /* CPU SIGNALS (Z84C0004 timings) */
 // assign #85 n_rd = n_rd_cpu; //TdCf(RDf)